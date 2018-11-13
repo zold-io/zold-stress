38f4bc2e09073789629c78cc23ef9ce259325492
Yegor Bugayenko <yegor256@gmail.com>
2018-11-13 14:15:37 +0300

    #5 eventmachine

--- test/zold/stress/test_pool.rb
+++ test/zold/stress/test_pool.rb
@@ -20,6 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+require 'eventmachine'
 require 'minitest/autorun'
 require 'zold/key'
 require 'zold/id'
--- test/zold/stress/test_stats.rb
+++ test/zold/stress/test_stats.rb
@@ -20,6 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+require 'eventmachine'
 require 'minitest/autorun'
 require_relative '../test__helper'
 require_relative '../../../lib/zold/stress/stats'
